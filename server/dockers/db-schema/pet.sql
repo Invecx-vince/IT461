eb43f0a7ef4ddfb5a71fcea6ab57635b15be5aac
Vince Xavier <vincexavierdelrosario@gmail.com>
2022-04-28 11:15:16 +0800

    Exercise 1
    implemented /cats and /cats/<cat_id>

--- server/dockers/db-schema/pet.sql
+++ server/dockers/db-schema/pet.sql
@@ -8,3 +8,10 @@ CREATE TABLE dogs(
     PRIMARY KEY (id),
     INDEX (name)
 );
+
+CREATE TABLE dogs(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (name)
+);
