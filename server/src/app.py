6bc39181827d1750c2be71e36e68c4f0a5d9ceed
Vince Xavier <vincexavierdelrosario@gmail.com>
2022-05-02 15:16:19 +0800

    update Exercise1

--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint
 from v1.dog.router import DogRouter
+from v1.cat.router import CatRouter
 
 app = Flask(__name__)
 
@@ -7,6 +8,10 @@ bp_dogs = Blueprint('dogs', __name__, url_prefix='/v1/dogs')
 DogRouter.handler(bp_dogs)
 app.register_blueprint(bp_dogs)
 
+bp_cats = Blueprint('cats', __name__, url_prefix='/v1/cats')
+CatRouter.handler(bp_cats)
+app.register_blueprint(bp_cats)
+""""
 @app.route('/cats', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def cats():
     cat = Cat()
@@ -42,6 +47,6 @@ def cat(cat_id):
     if str(request.method).upper() == 'DELETE':
         return jsonify(cat_object.delete(cat_id))
     return jsonify(cat)
-
+"""
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=6000)
